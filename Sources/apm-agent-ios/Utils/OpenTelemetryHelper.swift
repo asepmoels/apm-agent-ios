007dfa3cb73e5d677c5e8e188380ad43b3ea1b4d
Asep Mulyana <asep.mulyana@danamon.co.id>
2025-03-12 14:13:51 +0700

    feat: disable certificate validation

--- Sources/apm-agent-ios/Utils/OpenTelemetryHelper.swift
+++ Sources/apm-agent-ios/Utils/OpenTelemetryHelper.swift
@@ -53,6 +53,7 @@ public class OpenTelemetryHelper {
       
         if config.collectorTLS {
              return ClientConnection.usingPlatformAppropriateTLS(for: group)
+                 .withTLS(certificateVerification: .none)
                  .connect(host: config.collectorHost, port: config.collectorPort)
          } else {
               return ClientConnection.insecure(group: group)
