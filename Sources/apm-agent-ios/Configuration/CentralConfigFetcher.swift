60d10871986cd8bb4e05e53e2a3b33a45b355ac8
Asep Mulyana <asep.mulyana@danamon.co.id>
2025-03-19 14:38:27 +0700

    feat: allow invalid server's certificate

--- Sources/apm-agent-ios/Configuration/CentralConfigFetcher.swift
+++ Sources/apm-agent-ios/Configuration/CentralConfigFetcher.swift
@@ -130,7 +130,8 @@ class CentralConfigFetcher {
 
     if let request = buildCentralConfigRequest() {
 
-      task = URLSession.shared.dataTask(with: request, completionHandler: { data, response, error in
+      let session = URLSession(configuration: .default, delegate: AllowInvalidSslSessionDelegate(), delegateQueue: nil)
+      task = session.dataTask(with: request, completionHandler: { data, response, error in
         if let error = error {
           self.logger.error("\(error.localizedDescription)")
           return
